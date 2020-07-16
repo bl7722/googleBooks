748c38070b67ff139764e1d7529f9c5e221a5791
Brandon Lopez <brandon.i.lopez77@gmail.com>
2020-07-15 23:50:41 -0500

    deleted the following: client/serc/ App.css, App.test.js, index.css, logo.svg, serviceWorker.js,setupTests.js. Modefied client/src/App.js,index.js, and on root level: package-lock.json, package.json

--- google-books/client/src/App.js
+++ google-books/client/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {BrowserRouter as Router, Route} from "react-router-dom"
+import Search from "../src/components/pages/search"
+import Saved from "../src/components/pages/saved"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    return (
+      <Router>
+          <Route exact path="/" component={Search}/>
+          <Route exact path="/saved" component ={Saved}/>
+      </Router>
+    );
 }
 
 export default App;
