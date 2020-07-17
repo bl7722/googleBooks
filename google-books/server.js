7c6c87ff6005faba8615f2ab6b57eaaa5a439c64
Brandon Lopez <brandon.i.lopez77@gmail.com>
2020-07-17 00:13:29 -0500

    updated server.js

--- google-books/server.js
+++ google-books/server.js
@@ -8,19 +8,20 @@ const routes = require("./routes");
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks"); //"mongodb://user1:password123!@ds051605.mlab.com:51605/heroku_qxvm61xm");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password123!@ds051605.mlab.com:51605/heroku_qxvm61xm");
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 // Define any API routes before this runs
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname,"build","index"));
+  res.sendFile(path.join(__dirname,"client","build","index"));
 });
 
 // Start the API server
