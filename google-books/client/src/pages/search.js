ebdd635bd5d7f9956ceb9d4da4678d90c43c055d
Brandon Lopez <brandon.i.lopez77@gmail.com>
2020-07-16 23:37:21 -0500

    updated pages/search.js, DeleteBtn/index.js,Created savedReturn/index.js

--- google-books/client/src/pages/search.js
+++ google-books/client/src/pages/search.js
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from "react";
-import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
-import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
-import { List, ListItem } from "../components/List";
 import { Input, FormBtn } from "../components/Form";
 
 function Search() {
@@ -77,22 +74,8 @@ function Search() {
             <Jumbotron>
               <h1>Books on Shelf</h1>
             </Jumbotron>
-            {books.length ? (
-              <List>
-                {books.map(book => (
-                  <ListItem key={book._id}>
-                    <Link to={"/books/" + book._id}>
-                      <strong>
-                        {book.title} by {book.author}
-                      </strong>
-                    </Link>
-                    <DeleteBtn onClick={() => deleteBook(book._id)} />
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
               <h3>Nothing to Show</h3>
-            )}
+
           </Col>
         </Row>
       </Container>
